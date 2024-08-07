f285011dfb5e3c3de6e1327ce546e4ad02f6f0f9
ecabigting <ericcabigting@outlook.com>
2024-08-07 22:37:20 +0400

    implement websockets

---
+++ client/src/index.js
@@ -0,0 +1,109 @@
+import { createRoot } from "react-dom/client";
+import React, { useEffect, useState } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Card, Avatar, Input, Typography } from "antd";
+
+const { Search } = Input;
+const { Text } = Typography;
+const client = new W3CWebSocket("ws://localhost:8080");
+
+export const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [username, setUsername] = useState("");
+	const [message, setMessages] = useState([]);
+
+	// Connect to the websocket server on
+	client.onopen = () => {
+		console.log("Connected to websocket");
+	};
+
+	useEffect(() => {
+		client.onmessage = (msg) => {
+			const dataFromServer = JSON.parse(msg.data);
+			console.log(`FROM SERVER:`, dataFromServer);
+			if (dataFromServer.type === "message") {
+				setMessages((message) => [...message, { msg: dataFromServer.msg, user: dataFromServer.user }]);
+			}
+		};
+	}, []);
+
+	const onClickHandler = (val) => {
+		// using the client
+		// send the message as
+		// a json object with type message
+		// with the following params:
+		// message : the message we want to send
+		// user: the username of the user
+		client.send(
+			JSON.stringify({
+				type: "message",
+				msg: val,
+				user: username,
+			})
+		);
+	};
+
+	const joinChatHandler = (val) => {
+		setIsLoggedIn(true);
+		setUsername(val);
+	};
+	return (
+		<div className='main'>
+			{isLoggedIn ? (
+				<div>
+					<div className='title'>
+						<Text type='secondary' style={{ fontSize: "37px" }}>
+							Appliance Communicator
+						</Text>
+					</div>
+					<div style={{ display: "flex", flexDirection: "column", paddingBottom: "50px" }}>
+						{message.map((msg) => (
+							<p
+								style={{
+									color: "white",
+									backgroundColor: `${username === msg.user ? "green" : "blue"}`,
+									textAlign: `${username === msg.user ? "right" : "left"}`,
+								}}
+							>
+								<span style={{ fontSize: "20px;" }}>{msg.msg}</span>
+								<br />
+								<span style={{ fontSize: "10px;" }}>{msg.user}</span>
+							</p>
+						))}
+					</div>
+					<div className='bottom'>
+						<Search
+							placeholder='Message'
+							enterButton='Send'
+							// value={this.state.searchVal}
+							size='large'
+							// onChange={(e) => this.setState({ searchVal: e.target.value })}
+							onSearch={(value) => onClickHandler(value)}
+						/>
+					</div>
+					{/* <button onClick={() => onClickHandler("Hello!")}>Send Message</button>
+					{message.map((msg) => (
+						<p>
+							msg: {msg.msg},user:{msg.user}
+						</p>
+					))}
+					{console.log("msgs:", message)} */}
+				</div>
+			) : (
+				<div style={{ padding: "200px 40px" }}>
+					<Search
+						placeholder='Enter Name:'
+						enterButton='Login'
+						size='large'
+						onSearch={(value) => joinChatHandler(value)}
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
---
+++ server/index.js
@@ -0,0 +1,60 @@
+// Port
+const webSocketServerPort = 8080;
+// Loading the server
+const webSocketServer = require("websocket").server;
+// Loading HTTP
+const http = require("http");
+
+const server = http.createServer();
+server.listen(webSocketServerPort);
+console.log(`listening to port ${webSocketServerPort}`);
+
+// Spawn a webSocket server using the instance
+// of the HTTP server we created
+const wsServer = new webSocketServer({
+	httpServer: server,
+});
+
+// Where to store all connected clients
+const clients = {};
+
+// Define the request method for
+// the webSockets server and
+// what to do with tha request
+wsServer.on("request", function (req) {
+	var userID = getUniqueID();
+	// log the new connection request
+	console.log(`${new Date()} Recieved a new connection from origin ${req.origin}.`);
+
+	//accept the new connection request
+	//todo: filter request to only allowed origins
+	const connection = req.accept(null, req.origin);
+
+	// add the new connection to the clients list
+	clients[userID] = connection;
+
+	//log the new user in the connection list
+	console.log(`connected: ${userID} in ${Object.getOwnPropertyDescriptor(clients[userID])}`);
+
+	// message handler
+	connection.on("message", function (msg) {
+		if (msg.type === "utf8") {
+			console.log("Received Msg:", msg.utf8Data);
+
+			// broadcast the message to all connected clients
+			for (key in clients) {
+				clients[key].sendUTF(msg.utf8Data);
+				// console.log("Sent message to :", clients);
+			}
+		}
+	});
+});
+
+// Generate a Unique ID
+const getUniqueID = () => {
+	const us4 = () =>
+		Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+	return us4() + us4() + "-" + us4();
+};
